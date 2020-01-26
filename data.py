366ed7987ac695e27eff8d395885f4fa2be10f9b
P W <przemo2048@gmail.com>
2020-01-26 14:13:29 +0100

    code cleaning + fixed get_cop function

--- data.py
+++ data.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import numpy as np
 import pandas as pd
 
+
 class DataController:
     def __init__(self, nchann, nsamp, fs):
         self.t_data = TimesData(nsamp)
@@ -11,7 +12,7 @@ class DataController:
         self.cnt = 0
         self.nsamp = nsamp
         self.nchann = nchann
-        self.concat = False
+        self.is_concat = False
 
         self.y_raw = []
         self.y_trans = []
@@ -45,7 +46,7 @@ class DataController:
         self.y_trans = np.concatenate(self.y_trans, axis=1)
         self.xy_cop = np.concatenate(self.xy_cop, axis=1)
         self.times = np.concatenate(self.times)
-        self.concat = True
+        self.is_concat = True
 
     def clear_data(self):
         self.y_raw = []
@@ -56,11 +57,11 @@ class DataController:
         self.t_data = TimesData(self.nsamp)
         self.meas_data = MeasurementsData(self.nchann, self.nsamp)
         self.cop_data = CopData(self.nsamp)
-        self.concat = False
+        self.is_concat = False
 
     def get_meas(self, nsamp):
-        slice = min(nsamp, self.cnt)
-        if not self.concat:
+        slice = min(nsamp, self.cnt) - 1 # last sample is uncertain
+        if not self.is_concat:
             return self.t_data.t[-slice:], self.meas_data.y_trans[:, -slice:]
         else:
             return self.times, self.y_trans
@@ -77,12 +78,11 @@ class DataController:
             return self.times, self.y_raw
 
     def get_cop(self, nsamp):
-        slice = min(nsamp, self.cnt)
-        return self.cop_data.xyc[:, -slice:]
-        # if self.run:
-        #     return self.cop_data.xyc[:, -nsamp:]
-        # else:
-        #     return self.xy_cop
+        slice = min(nsamp, self.cnt) - 1 # last sample is uncertain
+        if not self.is_concat:
+            return self.cop_data.xyc[:, -slice:]
+        else:
+            return self.xy_cop
 
     def get_times(self, nsamp):
         if self.run:
@@ -106,101 +106,48 @@ class DataController:
         self.y_trans = df[['press_' + str(i) for i in range(self.nchann)]].to_numpy().T
         self.xy_cop = df[['cop_x_r', 'cop_y_r', 'cop_x_l', 'cop_x_r', 'cop_x_all', 'cop_y_all']].to_numpy().T
         self.cnt = len(self.times)
-        self.concat = True
-    # def get_meas(self):
-    #     return self.meas_data.y_trans
-    #
-    # def get_meas_raw(self):
-    #     return self.meas_data.y_raw
-    #
-    # def get_times(self):
-    #     return self.t_data.t
+        self.is_concat = True
 
-# @dataclass
-# class MeasurementsData:
-#     def __init__(self, nch, max_nsamp=None):
-#         self.nch = nch
-#         self.nsamp = 0
-#         self.max_nsamp = max_nsamp
-#         self.y_raw = np.zeros((nch, max_nsamp), dtype=np.uint16)
-#         self.y_trans = np.zeros((nch, max_nsamp))
-#         self.sum_all = 500
-#
-#     def append(self, samples):
-#         R20kg = 10
-#         R50kg = 47
-#         R = np.array([R50kg, R20kg, R20kg, R50kg, R20kg, R20kg])
-#         Uwe = 1023
-#         a20kg = 267.5
-#         a50kg = 684.9
-#         a = np.array([a50kg, a20kg, a20kg, a50kg, a20kg, a20kg])
-#
-#         for i in range(self.nch):
-#             lastsamp = samples[i]
-#             self.y_raw[i, -1] = lastsamp
-#             self.y_trans[i, -1] = a[i] / (R[i] * (lastsamp + 1e-6) / (Uwe - lastsamp + 1e-6))
-#
-#         # self.y_raw = np.roll(self.y_raw, -1)
-#         # self.y_trans = np.roll(self.y_trans, -1)
-#         # self.y_raw[:, -1] = samples
-#         # self.y_trans[:, -1] = a / (R * (samples + 1e-6) / (Uwe - samples + 1e-6))
 
 @dataclass
 class MeasurementsData:
-    def __init__(self, nch, max_nsamp=None):
+    def __init__(self, nch, max_nsamp):
         self.nch = nch
         self.nsamp = 0
         self.max_nsamp = max_nsamp
         self.y_raw = np.zeros((nch, max_nsamp), dtype=np.uint16)
         self.y_trans = np.zeros((nch, max_nsamp))
-        self.sum_all = 0
-    # nch = 6
-    # nsamp = 10_000
-    # n = 0
-    # sum_all = 0
-    # y_raw: np.ndarray = np.zeros((nch, nsamp), dtype=np.uint16)
-    # y_trans: np.ndarray = np.zeros((nch, nsamp))
-    #
-    # def __init__(self, nch, max_nsamp=None):
-    #     self.nch = nch
 
     def append(self, samples):
         R20kg = 10
         R50kg = 47
         R = [R50kg, R20kg, R20kg, R50kg, R20kg, R20kg]
         Uwe = 1023
-        a20kg = 267.5
-        a50kg = 684.9
+        a20kg = 315
+        a50kg = 708
         a = [a50kg, a20kg, a20kg, a50kg, a20kg, a20kg]
 
         self.y_raw = np.roll(self.y_raw, -1)
         self.y_trans = np.roll(self.y_trans, -1)
 
-        self.sum_all = 0
-
         for i in range(self.nch):
-            lastsamp = int(samples[i])
+            lastsamp = samples[i]
             self.y_raw[i, -1] = lastsamp
             self.y_trans[i, -1] = a[i] / (R[i] * (lastsamp + 1e-6) / (Uwe - lastsamp + 1e-6))
-            self.sum_all += self.y_trans[i, -1]
+
 
 @dataclass
 class CopData:
-    def __init__(self, max_nsamp=None):
+    def __init__(self, max_nsamp):
         self.max_nsamp = max_nsamp
         self.xyc = np.zeros((6, max_nsamp))
     def append(self, meas_data):
-        # self.sum_all = 0
-        # for i in range(self.nch):
-        #     lastsamp = int(samples[i])
-        #     self.sum_all += self.y_trans[i, -1]
-
         nx_P, ny_P, nx_L, ny_L, nx_all, ny_all = 0, 1, 2, 3, 4, 5
         y = meas_data.y_trans
-        sum_all = meas_data.sum_all
         self.xyc = np.roll(self.xyc, -1)
         sum_P = sum(y[:2, -1])
         sum_L = sum(y[2:5, -1])
+        sum_all = sum_P + sum_L
         self.xyc[nx_P, -1] = (0.3 * y[0, -1] + 2.3 * y[1, -1] - 2.8 * y[2, -1]) / sum_P
         self.xyc[ny_P, -1] = (-7.1 * y[0, -1] + 6.3 * y[1, -1] + 8 * y[2, -1]) / sum_P
         self.xyc[nx_L, -1] = (-0.3 * y[3, -1] - 2.3 * y[4, -1] + 2.8 * y[5, -1]) / sum_L
@@ -213,17 +160,10 @@ class CopData:
 
 @dataclass
 class TimesData:
-    def __init__(self, max_nsamp=None):
+    def __init__(self, max_nsamp):
         self.max_nsamp = max_nsamp
-        if max_nsamp is not None:
-            self.t = np.zeros(max_nsamp)
-        else:
-            # self.t = []
-            self.t = np.zeros(0)
+        self.t = np.zeros(max_nsamp)
 
     def append(self, t):
-        if self.max_nsamp is not None:
-            self.t = np.roll(self.t, -1)
-            self.t[-1] = t
-        else:
-            self.t = np.append(self.t, t)
+        self.t = np.roll(self.t, -1)
+        self.t[-1] = t
